9f074a818f1548fc7f1dec5b080eee55e09599bd
Sy Tran Dung <sytrancsvn@gmail.com>
2025-03-11 08:00:01 +0700

    fix await env

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,7 @@
 import { Network } from "@orbs-network/ton-access";
 
 const config = {
-  network: await import.meta.env.VITE_APP_NETWORK as Network,
-  address: await import.meta.env.VITE_APP_CONTRACT_ADDRESS,
+  network: import.meta.env.VITE_APP_NETWORK as Network,
+  address: import.meta.env.VITE_APP_CONTRACT_ADDRESS,
 } as const;
 export default  config;
-console.log(config)
